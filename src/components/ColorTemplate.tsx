8eb49d656329752fac9e1a8367827ddfa7718fe8
TanTran <tan.tranhoang@haravan.com>
2024-04-09 21:35:33 +0700

    Done! 1. Color Combination Display

--- src/components/ColorTemplate.tsx
+++ src/components/ColorTemplate.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IColorItem } from '../interfaces'
+import { checkDone } from '../utils'
 interface IProps {
     colors?: IColorItem[]
 }
 export const ColorTemplate: React.FC<IProps> = (props) => {
     const { colors } = props
+    const [copiedItem, setCopiedItem] = useState('')
+
+    const onCopy = (hexCode: string) => {
+        setCopiedItem(hexCode)
+        // save on clipboard 
+        navigator.clipboard.writeText(hexCode);
+        alert("Copied: " + hexCode);
+    }
     return (
         <div className='color-template'>
             {colors?.map(c => {
-                return <div className="color-template_item" style={{ backgroundColor: c?.hex }} key={c?.hex}>
-                    <span>{c?.hex}</span>
+                const isCopiedItem = c?.hex === copiedItem
+                return <div
+                    key={c?.hex}
+                    className={`color-template_item`}
+                    style={{ backgroundColor: c?.hex }}
+                    onClick={() => onCopy(c?.hex)}
+                >
+                    <span>{isCopiedItem ? checkDone() : c?.hex}</span>
                 </div>
             })}
         </div>
